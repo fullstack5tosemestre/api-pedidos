a120af6101bfa7de7730cb2e03f07d7619caadc9
dainz <mbenjaminarancibia@gmail.com>
2026-05-13 11:44:17 -0400

    remove unused files

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-docker run -d \
-    --name api \
-    --rm \
-    -p 80:8081 \
-    --env-file .env \
-    xdainz/api-pedidos:latest
--- run_db.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-    --rm \
-    --name db \
-    -p 3306:3306 \
-    -v mysql_data:/var/lib/mysql \
-    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
-    -e MYSQL_DATABASE=db_pedidos \
-    mysql:latest 
